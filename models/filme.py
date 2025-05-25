89028cc770f142830f3cff7943b9ea15b659c603
aquillesf <aquilleslerman@gmail.com>
2025-05-24 21:17:43 -0300

    adicionado filme

---
+++ models/filme.py
@@ -0,0 +1,7 @@
+class Filme:
+    def __init__(self, titulo, ano, genero, diretor, descricao=""):
+        self.titulo = titulo
+        self.ano = ano
+        self.genero = genero
+        self.diretor = diretor
+        self.descricao = descricao
